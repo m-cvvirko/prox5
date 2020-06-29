7e7551854d26df5df523cff54047ae28462afb1e
marekcwirko <marek.cwirko@hotmail.com>
2020-06-29 23:08:20 +0200

    corr to menu

--- src/features/prox5/Menu.js
+++ src/features/prox5/Menu.js
@@ -14,17 +14,21 @@ export const Menu = () => {
                 <div className="start selected-item">Start</div>
             </div>
             {/* <div className="menu-item" style="top:66px"> */}
-            <div className="menu-item" style={{top:'66px'}}>
+            <div className="menu-item" style={{top:'60px'}}>
                 <div className="your-cart">Your Cart</div>
             </div>
-            <div className="menu-item" style={{top:'90px'}}>
+            {/* <div className="menu-item" style={{top:'90px'}}> */}
+            <div className="menu-item" style={{top:'78px'}}>
                 <div className="favourites">Favourites</div>
             </div>
-            <div className="menu-item" style={{top:'115px'}}>
+            {/* <div className="menu-item" style={{top:'116px'}}> */}
+            <div className="menu-item" style={{top:'98px'}}>
                 <div className="your-orders">Your Orders</div>
             </div>
-            <div className="divider" style={{top:'171px'}}></div>
-            <div className="menu-item" style={{top:'226px'}}>
+            {/* <div className="divider" style={{top:'172px'}}></div> */}
+            <div className="divider" style={{top:'142px'}}></div>
+            {/* <div className="menu-item" style={{top:'226px'}}> */}
+            <div className="menu-item" style={{top:'190px'}}>
                 <div className="sign-out">Sign Out</div>
             </div>
         </div>
